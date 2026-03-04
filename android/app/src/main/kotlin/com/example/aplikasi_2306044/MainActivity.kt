c02b2d86d80e68475e13536d15c4a7e8bf1231a3
unknown <nurjakiah204@gmail.com>
2026-03-04 13:44:33 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/aplikasi_2306044/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aplikasi_2306044
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    String title = 'Aplikasi Pertamaku';
+    String name = 'Muhamad Rijki Nurjakiah';
+    String message = 'Hallo nama saya $name, selamat datang di $title';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(title),
+        ),
+        body: Column(
+          children: [
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Text(message),
+            Padding(padding: EdgeInsets.all(8.0),
+            child: Text(message))
+          ]
+        ),
+    ),
+    );
+  }
+
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
