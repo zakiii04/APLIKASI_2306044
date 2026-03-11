2a33b1791d992e25a2c93eaa793966756ef7cd95
unknown <nurjakiah204@gmail.com>
2026-03-11 10:15:31 +0700

    style widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,184 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    String title = 'Aplikasi Pertamaku';
-    String name = 'Muhamad Rijki Nurjakiah';
-    String message = 'Hallo nama saya $name, selamat datang di $title';
-
+class HomePage extends StatelessWidget{
+  build(context){
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange,
-          title: Text(title),
+          backgroundColor: Colors.grey,
+          leading: Icon(Icons.home),
+          title: Text('Aplikasi Pertamaku'),
         ),
-        body: Column(
-          children: [
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Text(message),
-            Padding(padding: EdgeInsets.all(8.0),
-            child: Text(message))
-          ]
+        body: SingleChildScrollView(
+          child:          Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            children:[
+              Row(
+                children: [
+                  Icon(Icons.archive),
+                  Text("artikel terbaru", style: TextStyle(fontWeight: .bold),)
+                ],
+              ),
+              Card(
+                child: Column(
+                crossAxisAlignment: .start,
+                children: [
+                  Image.network(
+                    'https://img.antaranews.com/cache/1200x800/2021/03/28/Exbfpl2WgAAQkl8.jpg.webp'
+                  ),
+                  Text("Nike gugat perusahaan pembuat 'Sepatu Setan' dengan Lil Nas X", style: TextStyle(fontWeight: .bold)),
+                  Text("Los Angeles (ANTARA) - Perusahaan pembuat sepatu atletik Nike Inc pada Senin (29/3)"),
+                  Text("menggugat perusahaan yang berbasis di New York yang memproduksi Satan Shoes (sepatu setan) yang konon mengandung setetes darah manusia.")
+                ],
+              ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Icon(Icons.comment),
+                  Text("Komentar",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: .bold
+                    ),
+                  )
+                ],
+              ),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Andi"),
+                          Text("Menurut saya langkah Nike menggugat itu wajar, karena sepatu tersebut menggunakan desain Nike tanpa izin resmi. Jika dibiarkan, bisa merusak citra brand mereka.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Siti"),
+                          Text("Kasus ini menarik karena memperlihatkan benturan antara seni, pemasaran, dan hak merek dagang. Nike tentu harus melindungi identitas produknya.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Budi"),
+                          Text("Saya rasa banyak orang mengira sepatu itu produk resmi Nike, padahal bukan. Jadi gugatan Nike memang penting agar tidak terjadi kesalahpahaman di masyarakat.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Rina"),
+                          Text("Kolaborasi Lil Nas X dengan MSCHF memang kreatif dan kontroversial, tetapi penggunaan logo Nike tanpa izin tetap menjadi masalah hukum.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Dika"),
+                          Text("Menurut saya ini strategi marketing yang sangat viral. Walaupun kontroversial, kasus ini justru membuat banyak orang membicarakan sepatu tersebut.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Lina"),
+                          Text("Saya memahami alasan Nike menggugat karena brand besar harus menjaga reputasinya. Apalagi tema “Satan Shoes” bisa memicu kontroversi di berbagai kalangan.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Rafi"),
+                          Text("Kasus ini menunjukkan betapa pentingnya hak kekayaan intelektual dalam industri fashion. Tanpa perlindungan hukum, brand bisa mudah disalahgunakan.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Maya"),
+                          Text("Di sisi lain, kolaborasi Lil Nas X juga merupakan bentuk ekspresi seni. Namun tetap harus menghormati aturan penggunaan merek.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Fajar"),
+                          Text("Menurut saya keputusan Nike tepat, karena banyak konsumen bisa mengira bahwa Nike mendukung konsep sepatu tersebut.")
+                        ]
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column (
+                        crossAxisAlignment: .start,
+                        children: [
+                          Text("Nina"),
+                          Text("Kasus ini menjadi pelajaran bahwa kreativitas dalam pemasaran tetap harus memperhatikan aspek hukum dan etika penggunaan merek.")
+                        ]
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
-    ),
-    );
-  }
+        )
 
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
